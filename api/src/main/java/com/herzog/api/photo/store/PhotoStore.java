358b0ef012841ae27971964741dc263aa40ba4a3
Ben Vanberg <bvanberg@gmail.com>
2015-11-22 11:09:38 -0700

    Ghetto paging.
    Allows for page size and page query params.

--- api/src/main/java/com/herzog/api/photo/store/PhotoStore.java
+++ api/src/main/java/com/herzog/api/photo/store/PhotoStore.java
@@ -1,8 +1,12 @@
 package com.herzog.api.photo.store;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.util.Collection;
+import java.util.List;
 
 /**
  * Simple photo store implementation. Eventually this should be backed by a real store that sends back real images.
@@ -60,4 +64,10 @@ public class PhotoStore {
             Photo.builder().url("https://unsplash.it/200/300/?random").build(),
             Photo.builder().url("https://unsplash.it/200/300/?random").build(),
             Photo.builder().url("https://unsplash.it/200/300/?random").build());
+
+    public Collection<Photo> getPhotoPage(final int page, final int pageSize) {
+        final Iterable<List<Photo>> partitions = Iterables.partition(photoList, pageSize);
+
+        return Iterables.get(partitions, page, photoList);
+    }
 }
